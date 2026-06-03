700005fbe8c814dcf7bf82ed955b1d07fb73d449
Anas <l.l.anas.l.looo@gmail.com>
2026-06-03 15:39:43 +0530

    11

--- src/app/layout/layout/layout.ts
+++ src/app/layout/layout/layout.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
 import { RouterOutlet, RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-layout',
-  imports: [RouterOutlet,RouterLink],
+  imports: [RouterOutlet, RouterLink, CommonModule],
   templateUrl: './layout.html',
   styleUrl: './layout.css',
 })
-export class Layout {}
+export class Layout {
+  showMenu = false;
+  sidebarOpen = true; // default open
+
+  toggleMenu() {
+    this.showMenu = !this.showMenu;
+  }
+
+  toggleSidebar() {
+    this.sidebarOpen = !this.sidebarOpen;
+  }
+
+  logout() {
+    alert('Logout clicked');
+  }
+}
