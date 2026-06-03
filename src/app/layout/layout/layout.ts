8791dfd0c89e7d168f4c17d0d2e44bbd7580dbc4
Anas <l.l.anas.l.looo@gmail.com>
2026-06-03 16:26:04 +0530

    12

--- src/app/layout/layout/layout.ts
+++ src/app/layout/layout/layout.ts
@@ -1,16 +1,27 @@
 import { Component } from '@angular/core';
-import { RouterOutlet, RouterLink } from '@angular/router';
+import {
+  RouterOutlet,
+  RouterLink,
+  RouterLinkActive
+} from '@angular/router';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-layout',
-  imports: [RouterOutlet, RouterLink, CommonModule],
+  standalone: true,
+  imports: [
+    RouterOutlet,
+    RouterLink,
+    RouterLinkActive,
+    CommonModule
+  ],
   templateUrl: './layout.html',
   styleUrl: './layout.css',
 })
 export class Layout {
+
   showMenu = false;
-  sidebarOpen = true; // default open
+  sidebarOpen = true;
 
   toggleMenu() {
     this.showMenu = !this.showMenu;
